233219f0a4bf886aeaf743b502f8036d4f249309
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-07-11 03:02:04 +0700

    subgraph updated

--- src/blog.ts
+++ src/blog.ts
@@ -1,50 +1,49 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Blog, PostCreated, PostUpdated } from "../generated/Blog/Blog"
-import { ExampleEntity } from "../generated/schema"
+import {
+  PostCreated as PostCreatedEvent,
+  PostUpdated as PostUpdatedEvent
+} from "../generated/Blog/Blog"
+import {
+  Post
+} from "../generated/schema"
+import { ipfs, json } from '@graphprotocol/graph-ts'
 
-export function handlePostCreated(event: PostCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handlePostCreated(event: PostCreatedEvent): void {
+  let post = new Post(event.params.id.toString());
+  post.title = event.params.title;
+  post.contentHash = event.params.hash;
+  post.published = true;
+  let data = ipfs.cat(event.params.hash);
+  if (data) {
+    let value = json.fromBytes(data).toObject()
+    if (value) {
+      const content = value.get('content')
+      if (content) {
+        post.postContent = content.toString()
+      }
+    }
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.id = event.params.id
-  entity.title = event.params.title
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.fetchPost(...)
-  // - contract.fetchPosts(...)
-  // - contract.name(...)
-  // - contract.owner(...)
+  post.createdAtTimestamp = event.block.timestamp;
+  post.save()
 }
 
-export function handlePostUpdated(event: PostUpdated): void {}
+export function handlePostUpdated(event: PostUpdatedEvent): void {
+  let post = Post.load(event.params.id.toString());
+  if (post) {
+    post.title = event.params.title;
+    post.contentHash = event.params.hash;
+    post.published = event.params.published;
+    post.published = true;
+    let data = ipfs.cat(event.params.hash);
+    if (data) {
+      let value = json.fromBytes(data).toObject()
+      if (value) {
+        const content = value.get('content')
+        if (content) {
+          post.postContent = content.toString()
+        }
+      }
+    }
+    post.updatedAtTimestamp = event.block.timestamp;
+    post.save()
+  }
+}
